d9cb6bd61a97636a37fffc52e42285a415fffc8a
Arthur Baia <arthurceminbaia@gmail.com>
2023-06-12 11:58:46 -0300

    client timeout

--- udp2/client/client.c
+++ udp2/client/client.c
@@ -9,11 +9,14 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include "client_manager.c"
+#include <netinet/ip.h>
+#include <sys/time.h>
 
-#define SERVERPORT "4950"    // the port users will be connecting to
+#define SERVERPORT "4950" // the port users will be connecting to
 #define MAXDATASIZE 100
 #define MAX_LEN_RCV 16384
 #define MAXLINE 1024
+#define TIMEOUT_MS 5000 // 5 seconds
 
 int main(int argc, char *argv[])
 {
@@ -22,8 +25,9 @@ int main(int argc, char *argv[])
     int rv;
     int numbytes;
 
-    if (argc != 2) {
-        fprintf(stderr,"usage: talker hostname message\n");
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: talker hostname message\n");
         exit(1);
     }
 
@@ -31,15 +35,18 @@ int main(int argc, char *argv[])
     hints.ai_family = AF_UNSPEC; // set to AF_INET to use IPv4
     hints.ai_socktype = SOCK_DGRAM;
 
-    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
     // loop through all the results and make a socket
-    for(p = servinfo; p != NULL; p = p->ai_next) {
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+                             p->ai_protocol)) == -1)
+        {
             perror("talker: socket");
             continue;
         }
@@ -47,7 +54,18 @@ int main(int argc, char *argv[])
         break;
     }
 
-    if (p == NULL) {
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_MS / 1000;           // seconds
+    timeout.tv_usec = (TIMEOUT_MS % 1000) * 1000; // microseconds
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1)
+    {
+        perror("talker: setsockopt");
+        return 3;
+    }
+
+    if (p == NULL)
+    {
         fprintf(stderr, "talker: failed to create socket\n");
         return 2;
     }
@@ -61,12 +79,12 @@ int main(int argc, char *argv[])
 
     while (option != 9)
     {
-        option = get_option(); //option chosen by the user
-        char str[MAXDATASIZE]; //string with info to be send
+        option = get_option(); // option chosen by the user
+        char str[MAXDATASIZE]; // string with info to be send
 
-        switch (option) 
+        switch (option)
         {
-        case 1: //create profile
+        case 1: // create profile
             printf("\n");
             create_profile(sockfd, p);
             printf("\n");
@@ -84,37 +102,37 @@ int main(int argc, char *argv[])
             general_function(sockfd, str, p, option);
             break;
 
-        case 4: //search profile by course
+        case 4: // search profile by course
             printf("Insert course:\n");
             scanf(" %[^\n]", str);
             general_function(sockfd, str, p, option);
             break;
 
-        case 5: //search profile by skill
+        case 5: // search profile by skill
             printf("Insert skill:\n");
             scanf(" %[^\n]", str);
             general_function(sockfd, str, p, option);
             break;
 
-        case 6: //search profile by grad year
+        case 6: // search profile by grad year
             printf("Insert graduation year:\n");
             scanf(" %[^\n]", str);
             general_function(sockfd, str, p, option);
             break;
 
-        case 7: //get all profiles
+        case 7: // get all profiles
             printf("Profiles:\n");
             strcpy(str, "all"); // just to consume the send opt
             general_function(sockfd, str, p, option);
             break;
 
-        case 8: //download image
+        case 8: // download image
             printf("Insert profile email:\n");
             scanf(" %[^\n]", str);
             download_image(sockfd, str, p, option);
             break;
 
-        case 9: //exit
+        case 9: // exit
             break;
 
         default:
